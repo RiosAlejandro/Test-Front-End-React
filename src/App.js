8ee80f526449a04977ce06e91726eeaaa2c07121
AlejandroRios <alexandro.aa86@gmail.com>
2022-07-02 19:49:48 -0300

    resolviendo conflicto en el merge

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Footer from './Components/footer/Footer.tsx';
-=======
 import Header from './Components/Header/Header.tsx';
->>>>>>> header
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-        <Footer />
-=======
+        
       <Header />
->>>>>>> header
+      <Footer />
     </>
   );
 }
