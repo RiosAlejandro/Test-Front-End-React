c411b50a8e74cf6c7bfe93ba899672829beb5a0f
AlejandroRios <alexandro.aa86@gmail.com>
2022-07-02 20:10:21 -0300

    inicio navegacion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navegacion from './Components/navegacion/Navegacion';
 
 function App() {
   return (
     <>
-
+      <Navegacion />
     </>
   );
 }
